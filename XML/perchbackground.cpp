ea1d137e4357863e0bbdc07b83edcbbf63ca68f1
chien <chien@potiondesign.com>
2011-08-09 18:35:42 -0400

    Jared add a static parameter to implement the substract function

---
+++ XML/perchbackground.cpp
@@ -0,0 +1,9 @@
+//
+//  background.cpp
+//  XML
+//
+//  Created by chien on 8/9/11.
+//  Copyright 2011 Potion Design. All rights reserved.
+//
+
+#include <iostream>
---
+++ XML/perchbackground.h
@@ -0,0 +1,14 @@
+//
+//  background.h
+//  XML
+//
+//  Created by chien on 8/9/11.
+//  Copyright 2011 Potion Design. All rights reserved.
+//
+
+#ifndef XML_background_h
+#define XML_background_h
+
+
+
+#endif
